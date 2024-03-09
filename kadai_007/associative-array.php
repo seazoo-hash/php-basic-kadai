b98ab167d000ae48221ead5522556ecc0e39dac6
seazoo-hash <155947973+seazoo-hash@users.noreply.github.com>
2024-03-09 22:09:47 +0900

    課題２：連想配列を作って値とキーを出力しよう

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+ <html lang="ja">
+ 
+    <head>
+        <meta charset="UTF-8">
+        <title>課題：テストの平均点を計算しよう</title>
+    </head>
+    
+    <body>
+        <p>
+            <?php
+            // 連想配列を作成し、変数に代入
+            $associative = ['name' => 'orion', 'price' => 200, 'weight' => 160];
+    
+            // 値を出力
+            print_r($associative);
+            ?>
+        </p>
+    </body>
+ 
+ </html>
